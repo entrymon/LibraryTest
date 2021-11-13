2d0c618afe42ec1c4faa24385accee365e399bf0
Amruta <amruta.g@cursivetech.net>
2021-11-13 13:11:58 +0530

    first commit

---
+++ src/main/java/com/cursivetech/test/libraryapp/BookService.java
@@ -0,0 +1,11 @@
+package com.cursivetech.test.libraryapp;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    
+	public void sortBooks() {
+		System.out.println("In Sort books");
+	}
+}
---
+++ src/main/java/com/cursivetech/test/libraryapp/LibraryAppApplication.java
@@ -0,0 +1,25 @@
+package com.cursivetech.test.libraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("Test application Started");
+		ApplicationContext applicationContext = SpringApplication.run(LibraryAppApplication.class, args);
+		BookService bookService = applicationContext.getBean(BookService.class);;
+		
+		try {
+			bookService.sortBooks();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
